e6c8e43a94e63995eb4fcb6240d3aa3bba44045a
Shafkatahmad <shafkataahmad@gmail.com>
2025-08-16 19:17:37 +0600

    js fundamentals

--- fundamentals.js
+++ fundamentals.js
@@ -3,11 +3,50 @@ const fatherName = 'Arnold';
 let season = 'rainy';
 season = 'winter';
 
+// 2.
 // 6 basic condition: >, <, ===, !==, <=, >=
 // multiple conditions: &&, ||
 if(fatherName === 'arnold' || season === 'rainy') {
 
 }
-else {
-  
+else if(fatherName === 'Arnold') {
+
 }
+else {
+
+}
+
+
+// 3. array declare
+// index
+// length, push, 
+const numbers = [89, 35, 89, 12];
+numbers[0] = 56;
+
+// 4. for loop
+for(let i = 0; i<numbers.length; i++) {
+  const number = numbers[i];
+  console.log(number);
+}
+
+// 5. function 
+function multiply(num1, num2) {
+  const result = num1 * num2;
+  return result;
+}
+
+const output = multiply(35, 78);
+
+// 6. Object
+// three ways to access property by name
+const student = {
+  name: 'Salib Khan',
+  age: 32,
+  movies: ['king', 'dhaka']
+}
+
+const myVariable = 'age';
+
+console.log(student.age); // direct by property
+console.log(student['age']); // access via property name string
+console.log(student[myVariable]); // access via property name in a variable
